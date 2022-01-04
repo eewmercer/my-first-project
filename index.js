fce9673aa81c46c852693ac1a310affe4e416fb8
eewmercer <eewmercer@gmail.com>
2022-01-04 18:30:36 -0500

    adds animals

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello World!?");
+if (process.env.noodle == "true") {
+  console.log("Noodle is true");
+} else {
+  console.log("Noodle is false");
+}
+const animals = ["pigs", "noodles", "poopooman", "cats", "dogs"];
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
