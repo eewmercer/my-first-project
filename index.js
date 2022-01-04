fa5b5b57892a58a72d39c21a561a6f780fea6a15
clayton.mercer <clayton.mercer@lifeomic.com>
2022-01-04 18:27:59 -0500

    HEHE

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!?");
+// console.log("Hello World!?");
+// console.log(process.env.noodle);
+// if (process.env.noodle == "true") {
+//   console.log("noodle is true");
+// } else {
+//   console.log("noodle is false");
+// }
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
+}
